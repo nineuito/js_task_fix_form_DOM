92f815e181f1ba948df9b8b9df638ddf268821d6
Oleh Chernov <nineuito@mail.com>
2024-12-31 13:47:33 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write code here
+const nodesFields = document.querySelectorAll('.field');
+
+const arrayNodesFields = [...nodesFields];
+
+arrayNodesFields.forEach((el) => {
+  const inputElement = el.querySelector('input');
+  const inputNameId = inputElement.getAttribute('id');
+  const inputName = inputElement.getAttribute('name');
+
+  const label = document.createElement('label');
+
+  label.setAttribute('for', inputNameId);
+  label.classList.add('field-label');
+  label.textContent = inputName;
+
+  el.insertBefore(label, inputElement);
+
+  function formatNamePlaceholder(inputNamePlaceholder) {
+    const words = [];
+    let currentWord = '';
+
+    for (let i = 0; i < inputNamePlaceholder.length; i++) {
+      const char = inputNamePlaceholder[i];
+
+      if (char.toUpperCase() === char && char !== char.toLowerCase()) {
+        if (currentWord) {
+          words.push(currentWord);
+        }
+
+        currentWord = char;
+      } else {
+        currentWord += char;
+      }
+    }
+    words.push(currentWord);
+
+    return words
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+
+  inputElement.setAttribute('placeholder', formatNamePlaceholder(inputName));
+});
